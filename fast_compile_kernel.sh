7e54fdd73e778b1d85664f38b04f1c55d61710d3
guohui <guohui@guohui-desktop.(none)>
2011-05-09 22:52:07 +0800

    modify part of file according talon kernel

--- fast_compile_kernel.sh
+++ fast_compile_kernel.sh
@@ -1,7 +1,9 @@
 #/bin/sh
 . ./setenv.sh
 chmod 4755 ../initramfs-GB-GT-I9000/root/sbin/su
+VER=`cat .version`
 make -j4
+echo $VER > .version
 find . -name *.ko | while read MODULE; do cp $MODULE ../initramfs-GB-GT-I9000/root/lib/modules/ ; done
 make -j4
 
